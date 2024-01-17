26b4e0688a3456625f1247faee510dc8cb8b9a16
Anurag Maldhure <hello.anurag.maldhure@gmail.com>
2024-01-17 22:19:17 +0530

    Created Interface to be implemented

---
+++ src/main/java/com/anurag/miniURL/ShortURLImpl.java
@@ -0,0 +1,29 @@
+package com.anurag.miniURL;
+
+public class ShortURLImpl implements ShortURL{
+    @Override
+    public String registerNewUrl(String longUrl) {
+        return null;
+    }
+
+    @Override
+    public String registerNewUrl(String longUrl, String shortUrl) {
+        return null;
+    }
+
+    @Override
+    public String getUrl(String shortUrl) {
+        return null;
+    }
+
+    @Override
+    public Integer getHitCount(String longUrl) {
+        return null;
+    }
+
+    @Override
+    public String delete(String longUrl) {
+        return null;
+    }
+}
+
---
+++ src/main/java/com/anurag/tester/MainApplication.java
@@ -0,0 +1,9 @@
+package com.anurag.tester;
+
+import com.anurag.miniURL.*;
+
+public class MainApplication {
+    public static void main(String[] args){
+      
+    }
+}
